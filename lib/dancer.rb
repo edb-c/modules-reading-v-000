e431a99b4dae17a33e92bdf0e06ff585ee1ff676
Ethel C <cofresi.ethel@gmail.com>
2018-07-27 13:05:02 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  # include Dance
+  include Dance
   # extend MetaDancing
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
